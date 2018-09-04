0f47d5cbd8d999cbe63854747968119a15dd2736
PeileiWang <o6996o@163.com>
2018-09-04 11:05:05 +0800

    新增facade模式例子

--- facade/src/main/java/facade/Test.java
+++ facade/src/main/java/facade/Test.java
@@ -2,13 +2,11 @@ package facade;
 
 import facade.facade.Computer;
 
-
 /**
  * Created by wangpeilei
  * Time 2018/9/4 10:35
  */
 public class Test {
-
     public static void main(String[] args) {
         Computer computer = new Computer();
         computer.start();
